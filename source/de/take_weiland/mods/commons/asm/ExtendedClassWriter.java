b5fae24d2ae50b6b65a9f1ca98ec63e89240a820
diesieben07 <diesieben07@gmail.com>
2013-08-28 16:22:13 +0200

    Attempt to fix the getCommonSuperclass again

--- source/de/take_weiland/mods/commons/asm/ExtendedClassWriter.java
+++ source/de/take_weiland/mods/commons/asm/ExtendedClassWriter.java
@@ -1,7 +1,9 @@
 package de.take_weiland.mods.commons.asm;
 
 import org.objectweb.asm.ClassWriter;
+import static org.objectweb.asm.Opcodes.*;
 import org.objectweb.asm.tree.ClassNode;
+
 
 public class ExtendedClassWriter extends ClassWriter {
 
@@ -14,12 +16,28 @@ public class ExtendedClassWriter extends ClassWriter {
 		try {
 			return super.getCommonSuperClass(type1, type2);
 		} catch (RuntimeException e) {
-			if (isAssignableFrom(type1, ASMUtils.getClassNode(type2))) {
+			ClassNode node1;
+			ClassNode node2;
+			if (isAssignableFrom(type1, (node1 = ASMUtils.getClassNode(type2)))) {
 				return type1;
-			} else if (isAssignableFrom(type2, ASMUtils.getClassNode(type1))) {
+			}
+			if (isAssignableFrom(type2, (node2 = ASMUtils.getClassNode(type1)))) {
 				return type2;
 			}
-			throw new RuntimeException("Failed to get common superclass for " + type1 + " & " + type2, e);
+			if ((node1.access & ACC_INTERFACE) == ACC_INTERFACE || (node2.access & ACC_INTERFACE) == ACC_INTERFACE) {
+				return "java/lang/Object";
+			} else {
+				do {
+					String superName = node1.superName;
+					if (superName.equals("java/lang/Object")) {
+						return "java/lang/Object";
+					}
+					node1 = ASMUtils.getClassNode(node1.superName);
+				} while (!isAssignableFrom(node1.name, node2));
+				return node1.name;
+			}
+		
+//			throw new RuntimeException("Failed to get common superclass for " + type1 + " & " + type2, e);
 		}
 	}
 
