3bbab8db46d24dded9e875841af328c69b55723f
diesieben07 <diesieben07@gmail.com>
2014-03-01 13:15:38 +0100

    Document @ToNbt

--- src/main/java/de/take_weiland/mods/commons/nbt/ToNbt.java
+++ src/main/java/de/take_weiland/mods/commons/nbt/ToNbt.java
@@ -6,12 +6,27 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * <p>Apply this to a field in your {@link net.minecraft.tileentity.TileEntity}, {@link net.minecraft.entity.Entity} or {@link net.minecraftforge.common.IExtendedEntityProperties}
+ * to automatically save and load it from NBT.</p>
+ * <p>Supports all primitives as well as Enums and Strings and arrays thereof, even if not directly supported by NBT.</p>
+ * <ul>
+ *     <li>Primitives, Strings, int[], byte[] are saved as-is with the corresponding methods in {@link net.minecraft.nbt.NBTTagCompound}</li>
+ *     <li>boolean[] are saved as a byte[] of bit flags (every byte contains up to 8 booleans). For any key, e.g. "someKey" it also generates the integer "someKey_sc$boolArrLen"
+ *         to accurately reconstruct the array.</li>
+ *     <li>long[] are saved as an int[] of twice the size, two integers making up one long.</li>
+ *     <li>float[] and double[] are saved as an int[] resp. long[] after applying {@link java.lang.Float#floatToIntBits(float)} resp. {@link java.lang.Double#doubleToLongBits(double)}</li>
+ *     <li>String[] are saved as a {@link net.minecraft.nbt.NBTTagList} containing the Strings</li>
+ *     <li>Enums and Enum[] are saved as a String resp. String[] containing the names of the Enum constants ({@link Enum#name()}</li>
+ * </ul>
  * @author diesieben07
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface ToNbt {
 
+	/**
+	 * the key to use for this field. Empty String (default) uses the field name
+	 */
 	String value() default "";
 
 }
