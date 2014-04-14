87ec00f246fb9215705e3700f7e9c398664c2181
diesieben07 <diesieben07@gmail.com>
2014-04-14 17:24:19 +0200

    remove debug code

--- src/main/java/de/take_weiland/mods/commons/meta/AbstractArrayProperty.java
+++ src/main/java/de/take_weiland/mods/commons/meta/AbstractArrayProperty.java
@@ -2,19 +2,27 @@ package de.take_weiland.mods.commons.meta;
 
 import de.take_weiland.mods.commons.util.JavaUtils;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 /**
  * @author diesieben07
  */
-abstract class AbstractArrayProperty<T> extends AbstractProperty<T> {
+abstract class AbstractArrayProperty<T> extends GenericProperty<T> {
 
 	private final T[] values;
+	private final int shift;
+	private final int mask;
 
 	AbstractArrayProperty(int shift, T[] values) {
-		super(shift, values.length);
+		checkArgument(values.length >= 2, "Must have at least 2 elements!");
+		this.shift = shift;
+		int bitCount = Integer.numberOfTrailingZeros(Integer.highestOneBit(values.length - 1)) + 1;
+		System.out.println("bitCount: " + bitCount);
+		this.mask = (1 << bitCount) - 1;
+		System.out.println(this + " has mask " + Integer.toBinaryString(mask));
 		this.values = values;
 	}
 
-	@Override
 	T value0(int metadata) {
 		return JavaUtils.get(values, metadata);
 	}
@@ -22,4 +30,16 @@ abstract class AbstractArrayProperty<T> extends AbstractProperty<T> {
 	final T[] values() {
 		return values;
 	}
+
+	@Override
+	public final T value(int metadata) {
+		return value0((metadata >> shift) & mask);
+	}
+
+	@Override
+	public final int toMeta(T value, int previousMeta) {
+		return previousMeta | (toMeta0(value) << shift);
+	}
+
+	abstract int toMeta0(T value);
 }
