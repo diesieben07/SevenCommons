05355b91a2a575dd422cb5c81a97d97b7b6525c5
Parker Young <pyoung1998@gmail.com>
2014-05-26 18:48:31 -0400

    Minor Javadoc stuff

--- src/main/java/de/take_weiland/mods/commons/Listenable.java
+++ src/main/java/de/take_weiland/mods/commons/Listenable.java
@@ -1,11 +1,11 @@
 package de.take_weiland.mods.commons;
 
 /**
- * <p>Simple Interface for implementing the <a href="http://en.wikipedia.org/wiki/Observer_pattern">Observer Pattern</a>.</p>
- * <p>Use the methods in {@link de.take_weiland.mods.commons.Listenables} to register and unregister listeners.</p>
- * <p>To notify listeners, call {@link de.take_weiland.mods.commons.Listenables#onChange(Listenable)}, which will then call
+ * Simple Interface for implementing the <a href="http://en.wikipedia.org/wiki/Observer_pattern">Observer Pattern</a>.<br/>
+ * Use the methods in {@link de.take_weiland.mods.commons.Listenables} to register and unregister listeners.<br/>
+ * To notify listeners, call {@link de.take_weiland.mods.commons.Listenables#onChange(Listenable)}, which will then call
  * {@link de.take_weiland.mods.commons.Listenable.Listener#onChange(Object)} on every listener and also {@link Listenable#onChange()}
- * on the Listenable itself.</p>
+ * on the Listenable itself.<br/>
  * @author diesieben07
  */
 public interface Listenable<SELF extends Listenable<SELF>> {
