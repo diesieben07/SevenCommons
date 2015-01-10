065458c32c9df040522f678ace66ac57badc3e20
diesieben07 <diesieben07@gmail.com>
2015-01-11 00:44:38 +0100

    Javadocs for @Watcher.Provider

--- src/main/java/de/take_weiland/mods/commons/serialize/ByteStreamSerializer.java
+++ src/main/java/de/take_weiland/mods/commons/serialize/ByteStreamSerializer.java
@@ -59,9 +59,11 @@ public interface ByteStreamSerializer<T> {
 	 * <p>A Provider for ByteStreamSerializers.</p>
 	 * <p>When applied to a method, this method must be static and accept a single parameter of type
 	 * {@link de.take_weiland.mods.commons.serialize.TypeSpecification}. It's return type must be any non-primitive type.</p>
+	 *
 	 * <p>When applied to a field, this field must be static and final. It's type must be assignable to {@code ByteStreamSerializer}
 	 * resp. {@code ByteStreamSerializer.Contents}. The {@linkplain #method() SerializationMethod filter} must be set when applied
 	 * to fields.</p>
+	 *
 	 * <p>The TypeSpecification dictates what this provider must provide. The TypeSpecification will only ever report
 	 * the concrete SerializationMethods {@code VALUE} or {@code CONTENTS}. The provider must then return an instance of
 	 * {@code ByteStreamSerializer} or {@code ByteStreamSerializer.Contents} respectively. If this provider cannot provide a
@@ -73,9 +75,11 @@ public interface ByteStreamSerializer<T> {
 
 		/**
 		 * <p>The base type that this provider can provide serializers for. This provider will only be queried for that
-		 * type or subtypes. In other words: if this attribute reports {@code T.class}, this provider will only be queried for TypeSpecifications
-		 * of type {@code TypeSpecification&lt;? extends T&gt;}.</p>
+		 * type or subtypes. In other words: if this attribute reports {@code T.class}, this provider will only be queried
+		 * for TypeSpecifications of type {@code TypeSpecification&lt;? extends T&gt;}.</p>
+		 *
 		 * <p>If this method is left at default, the class declaring the provider will be used.</p>
+		 *
 		 * @return the base type
 		 */
 		// if name is changed, need to update SerializerRegistry as well!
@@ -84,7 +88,9 @@ public interface ByteStreamSerializer<T> {
 		/**
 		 * <p>A filter for the SerializationMethod that this provider supports. If {@code VALUE} or {@code CONTENTS}
 		 * are specified, this provider will only be queried for types that demand that SerializationMethod.</p>
+		 *
 		 * <p>A filter is mandatory for fields.</p>
+		 *
 		 * @return a filter
 		 */
 		SerializationMethod method() default SerializationMethod.DEFAULT;
--- src/main/java/de/take_weiland/mods/commons/sync/Watcher.java
+++ src/main/java/de/take_weiland/mods/commons/sync/Watcher.java
@@ -63,16 +63,41 @@ public interface Watcher<T> {
      */
     <OBJ> void read(MCDataInput in, SyncableProperty<T, OBJ> property, OBJ instance);
 
+    /**
+     * <p>A Provider for Watchers.</p>
+     *
+     * <p>When applied to a method, this method must be static and accept a single parameter of type
+     * {@link de.take_weiland.mods.commons.serialize.TypeSpecification}. It's return type must be any non-primitive type.</p>
+     *
+     * <p>When applied to a field, this field must be static and final. It's type must be assignable to {@code Watcher}.
+     * The {@linkplain #method() SerializationMethod filter} must be set when applied to fields.</p>
+     *
+     * <p>The TypeSpecification dictates what this provider must provide. The TypeSpecification will only ever report
+     * the concrete SerializationMethods {@code VALUE} or {@code CONTENTS}. The provider must then return an instance of
+     * {@code Watcher} that implements the given semantics. If this provider cannot provide a
+     * Watcher for the given TypeSpecification, {@code null} must be returned.</p>
+     */
     @Retention(RetentionPolicy.RUNTIME)
     @Target({ElementType.METHOD, ElementType.FIELD})
     @interface Provider {
 
+        /**
+         * <p>The base type that this provider can provide Watchers for. This provider will only be queried for that
+         * type or subtypes. In other words: if this attribute reports {@code T.class}, this provider will only be queried
+         * for TypeSpecifications of type {@code TypeSpecification&lt;? extends T&gt;}.</p>
+         *
+         * <p>If this method is left at default, the class declaring the provider will be used.</p>
+         *
+         * @return the base type
+         */
         // if name is changed, need to update SerializerRegistry as well!
         Class<?> forType() default AnnotationNull.class;
 
         /**
-         * <p>Specify a filter for the {@code SerializationMethod} this provider handles. {@code DEFAULT} means no filter.</p>
-         * <p>On fields this attribute is mandatory.</p>
+         * <p>A filter for the SerializationMethod that this provider supports. If {@code VALUE} or {@code CONTENTS}
+         * are specified, this provider will only be queried for types that demand that SerializationMethod.</p>
+         *
+         * <p>A filter is mandatory for fields.</p>
          *
          * @return a filter
          */
