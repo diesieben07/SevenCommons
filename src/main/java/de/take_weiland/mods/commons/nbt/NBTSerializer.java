7bcf56fce4111dc8597b97fab23691b31a947b6a
diesieben07 <diesieben07@gmail.com>
2015-05-23 17:50:16 +0200

    Remove unneeded methods in NBTSerializer

--- src/main/java/de/take_weiland/mods/commons/nbt/NBTSerializer.java
+++ src/main/java/de/take_weiland/mods/commons/nbt/NBTSerializer.java
@@ -3,9 +3,6 @@ package de.take_weiland.mods.commons.nbt;
 import de.take_weiland.mods.commons.reflect.PropertyAccess;
 import net.minecraft.nbt.NBTBase;
 
-import java.util.function.BiConsumer;
-import java.util.function.Function;
-
 /**
  * <p>A serializer to encode and decode values of type {@code T} to NBT.</p>
  *
@@ -13,18 +10,10 @@ import java.util.function.Function;
  */
 public interface NBTSerializer<T> {
 
-    default void read(NBTBase nbt, Object obj, PropertyAccess<T> property) {
-        read(nbt, obj, property, property);
-    }
-
-    void read(NBTBase nbt, Object obj, Function<Object, ? extends T> getter, BiConsumer<Object, ? super T> setter);
+    void read(NBTBase nbt, Object obj, PropertyAccess<T> property);
 
     default NBTBase write(Object obj, PropertyAccess<T> property) {
-        return write(obj, property, property);
-    }
-
-    default NBTBase write(Object obj, Function<Object, ? extends T> getter, BiConsumer<Object, ? super T> setter) {
-        T value = getter.apply(obj);
+        T value = property.apply(obj);
         return value == null ? NBTData.serializedNull() : write(value);
     }
 
@@ -35,11 +24,11 @@ public interface NBTSerializer<T> {
         T read(NBTBase nbt);
 
         @Override
-        default void read(NBTBase nbt, Object obj, Function<Object, ? extends T> getter, BiConsumer<Object, ? super T> setter) {
+        default void read(NBTBase nbt, Object obj, PropertyAccess<T> property) {
             if (NBTData.isSerializedNull(nbt)) {
-                setter.accept(obj, null);
+                property.set(obj, null);
             } else {
-                setter.accept(obj, read(nbt));
+                property.set(obj, read(nbt));
             }
         }
 
@@ -50,9 +39,9 @@ public interface NBTSerializer<T> {
         void read(NBTBase nbt, T value);
 
         @Override
-        default void read(NBTBase nbt, Object obj, Function<Object, ? extends T> getter, BiConsumer<Object, ? super T> setter) {
+        default void read(NBTBase nbt, Object obj, PropertyAccess<T> property) {
             if (!NBTData.isSerializedNull(nbt)) {
-                read(nbt, getter.apply(obj));
+                read(nbt, property.get(obj));
             }
         }
     }
