138f3aa404cd5a3e0d9f0ec0b80cc3e755b1490e
diesieben07 <diesieben07@gmail.com>
2015-05-29 22:49:02 +0200

    Revert "Add Player to PacketConstructor"
    This reverts commit de48303f2d5c758f1807f1d0f9737c304fabf52a.

--- src/main/java/de/take_weiland/mods/commons/net/PacketConstructor.java
+++ src/main/java/de/take_weiland/mods/commons/net/PacketConstructor.java
@@ -1,8 +1,7 @@
 package de.take_weiland.mods.commons.net;
 
-import net.minecraft.entity.player.EntityPlayer;
-
 import java.io.Serializable;
+import java.util.function.Function;
 
 /**
  * <p>A PacketConstructor is used to instantiate a Packet object when it is received.</p>
@@ -14,21 +13,9 @@ import java.io.Serializable;
  * @author diesieben07
  */
 @FunctionalInterface
-public interface PacketConstructor<P extends Packet> extends Serializable {
-
-    P construct(MCDataInput in, EntityPlayer player);
+public interface PacketConstructor<P extends Packet> extends Function<MCDataInput, P>, Serializable {
 
     default Class<P> getPacketClass() {
         return Network.findPacketClassReflectively(this);
-    }
-
-    interface Simple<P extends Packet> extends PacketConstructor<P> {
-
-        P construct(MCDataInput in);
-
-        @Override
-        default P construct(MCDataInput in, EntityPlayer player) {
-            return construct(in);
-        }
     }
 }
