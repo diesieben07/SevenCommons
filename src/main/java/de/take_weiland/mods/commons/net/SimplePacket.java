c63a5516c45bf7c644b2caa61fde92d480bc40e0
diesieben07 <diesieben07@gmail.com>
2015-06-17 09:26:43 +0200

    Packets, anyone? v2

--- src/main/java/de/take_weiland/mods/commons/net/SimplePacket.java
+++ src/main/java/de/take_weiland/mods/commons/net/SimplePacket.java
@@ -1,9 +1,11 @@
 package de.take_weiland.mods.commons.net;
 
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
+import de.take_weiland.mods.commons.internal.SCReflector;
 import de.take_weiland.mods.commons.util.Entities;
 import de.take_weiland.mods.commons.util.Players;
-import de.take_weiland.mods.commons.internal.SCReflector;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -13,6 +15,8 @@ import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 
 import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Predicate;
 
 /**
@@ -88,6 +92,83 @@ public interface SimplePacket {
         // the filter makes sure it only contains EntityPlayer's
         //noinspection unchecked
         sendTo(Iterables.filter(SCReflector.instance.getCrafters(c), EntityPlayer.class));
+    }
+
+    interface WithResponse<R> {
+
+        CompletableFuture<R> sendToServer();
+
+        CompletableFuture<R> sendTo(EntityPlayerMP player);
+
+        default CompletableFuture<R> sendTo(EntityPlayer player) {
+            return sendTo(Players.checkNotClient(player));
+        }
+
+        default Map<EntityPlayerMP, CompletableFuture<R>> sendTo(Iterable<? extends EntityPlayer> players, com.google.common.base.Predicate<? super EntityPlayerMP> filter) {
+            return FluentIterable.from(players)
+                    .transform(Players::checkNotClient)
+                    .filter(filter)
+                    .toMap(this::sendTo);
+
+            ImmutableMap.Builder<EntityPlayer, CompletableFuture<R>> b = ImmutableMap.builder();
+
+            for (EntityPlayer p : players) {
+                EntityPlayerMP player = Players.checkNotClient(p);
+                if (filter == null || filter.test(player)) {
+                    b.put(player, sendTo(player));
+                }
+            }
+            return b.build();
+        }
+
+        default Map<EntityPlayer, CompletableFuture<R>> sendTo(Iterable<? extends EntityPlayer> players) {
+            return sendTo(players, null);
+        }
+
+        default Map<EntityPlayer, CompletableFuture<R>> sendTo(Predicate<? super EntityPlayerMP> filter) {
+            return sendTo(Players.getAll(), filter);
+        }
+
+        default Map<EntityPlayer, CompletableFuture<R>> sendToAll() {
+            return sendTo(Players.getAll(), null);
+        }
+
+        default Map<EntityPlayer, CompletableFuture<R>> sendToAllIn(World world) {
+            return sendTo(Players.allIn(world), null);
+        }
+
+        default Map<EntityPlayer, CompletableFuture<R>> sendTo(World world, Predicate<? super EntityPlayerMP> filter) {
+            return sendTo(Players.allIn(world), filter);
+        }
+
+        default Map<EntityPlayer, CompletableFuture<R>> sendToAllNear(Entity entity, double radius) {
+            return sendToAllNear(entity.worldObj, entity.posX, entity.posY, entity.posZ, radius);
+        }
+
+        default Map<EntityPlayer, CompletableFuture<R>> sendToAllNear(TileEntity te, double radius) {
+            return sendToAllNear(te.getWorldObj(), te.xCoord, te.yCoord, te.zCoord, radius);
+        }
+
+        default Map<EntityPlayer, CompletableFuture<R>> sendToAllNear(World world, double x, double y, double z, double radius) {
+            double rSq = radius * radius;
+            return sendTo(Players.allIn(world), p -> p.getDistanceSq(x, y, z) <= rSq);
+        }
+
+        default Map<EntityPlayer, CompletableFuture<R>> sendToAllTracking(TileEntity te) {
+            return sendToAllTrackingChunk(te.getWorldObj(), te.xCoord >> 4, te.zCoord >> 4);
+        }
+
+        default Map<EntityPlayer, CompletableFuture<R>> sendToAllTracking(Chunk chunk) {
+            return sendToAllTrackingChunk(chunk.worldObj, chunk.xPosition, chunk.zPosition);
+        }
+
+        default Map<EntityPlayer, CompletableFuture<R>> sendToAllTrackingChunk(World world, int chunkX, int chunkZ) {
+            return sendTo(Players.getTrackingChunk(world, chunkX, chunkZ), null);
+        }
+
+        default Map<EntityPlayer, CompletableFuture<R>> sendToAllTracking(Entity entity) {
+            return sendTo(Entities.getTrackingPlayers(entity), null);
+        }
     }
 
 }
