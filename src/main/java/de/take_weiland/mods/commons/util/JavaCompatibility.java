96015037300d4a85c2892b1c3596b4a166330ab3
diesieben07 <diesieben07@gmail.com>
2015-08-22 18:32:37 +0200

    Make outdated java warning an actual window so it can't "disappear"

--- src/main/java/de/take_weiland/mods/commons/util/JavaCompatibility.java
+++ src/main/java/de/take_weiland/mods/commons/util/JavaCompatibility.java
@@ -1,6 +1,7 @@
 package de.take_weiland.mods.commons.util;
 
 import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.relauncher.FMLLaunchHandler;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 import org.apache.commons.lang3.JavaVersion;
 import org.apache.commons.lang3.StringUtils;
@@ -12,6 +13,10 @@ import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.lang.reflect.Method;
 
 /**
@@ -19,6 +24,7 @@ import java.lang.reflect.Method;
  *
  * @author diesieben07
  */
+@SuppressWarnings("Convert2Lambda")
 public final class JavaCompatibility {
 
     /**
@@ -35,57 +41,111 @@ public final class JavaCompatibility {
             return;
         }
 
+        final boolean displayExeNotice = SystemUtils.IS_OS_WINDOWS && FMLLaunchHandler.side().isClient();
+
         Logger logger = LogManager.getLogger("STDERR");
         logger.error("");
         logger.error(StringUtils.repeat('=', 80));
         logger.error("SevenCommons requires Java 8 to be installed.");
         logger.error("Please install the latest Java 8 appropriate for your System from https://java.com/download/" +
-                (SystemUtils.IS_OS_WINDOWS ? " or use the latest .exe launcher from https://minecraft.net/" : ""));
+                (displayExeNotice ? " or use the latest .exe launcher from https://minecraft.net/" : ""));
         logger.error("Thank you. The game will exit now.");
         logger.error(StringUtils.repeat('=', 80));
         logger.error("");
 
         if (!GraphicsEnvironment.isHeadless()) {
-            try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } catch (Exception ignored) {
+            final Object mutex = new Object();
 
-            }
-
-            JLabel label = new JLabel();
-            Font font = label.getFont();
-
-            // create some css from the label's font
-            StringBuilder style = new StringBuilder("font-family:" + font.getFamily() + ";")
-                    .append("font-weight:")
-                    .append(font.isBold() ? "bold" : "normal")
-                    .append(";")
-                    .append("font-size:")
-                    .append(font.getSize()).append("pt;");
-
-            JEditorPane ep = new JEditorPane("text/html", "<html><body style=\"" + style + "\">" +
-                    "<strong>SevenCommons requires Java 8 to be installed.</strong><br />" +
-                    "Please install the latest Java 8 appropriate for your System from <a href=\"https://java.com/download/\">java.com/download</a>" +
-                    " or use the latest .exe launcher from <a href=\"https://minecraft.net/\">minecraft.net</a>.<br /><br />" +
-                    "Thank you. The game will exit now." +
-                    "</body></html>");
-
-            //noinspection Convert2Lambda
-            ep.addHyperlinkListener(new HyperlinkListener() {
+            SwingUtilities.invokeLater(new Runnable() {
                 @Override
-                public void hyperlinkUpdate(HyperlinkEvent e) {
-                    if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED))
-                        try {
-                            Desktop.getDesktop().browse(e.getURL().toURI());
-                        } catch (Exception ignored) {
+                public void run() {
+                    try {
+                        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                    } catch (Exception ignored) {
+                    }
+
+                    JLabel label = new JLabel();
+                    Font font = label.getFont();
+
+                    // create some css from the label's font
+                    StringBuilder style = new StringBuilder("font-family:" + font.getFamily() + ";")
+                            .append("font-weight:")
+                            .append(font.isBold() ? "bold" : "normal")
+                            .append(";")
+                            .append("font-size:")
+                            .append(font.getSize()).append("pt;");
+
+                    JTextPane text = new JTextPane();
+                    text.setContentType("text/html");
+                    text.setText("<html><body style=\"" + style + "\">" +
+                            "<strong>SevenCommons requires Java 8 to be installed.</strong><br />" +
+                            "Please install the latest Java 8 appropriate for your System from <a href=\"https://java.com/download/\">java.com/download</a>" +
+                            (displayExeNotice ? " or use the latest .exe launcher from <a href=\"https://minecraft.net/\">minecraft.net</a>" : "") +
+                            ".<br /><br />Thank you. The game will exit now." +
+                            "</body></html>");
+
+                    text.setEditable(false);
+                    text.setHighlighter(null);
+                    text.setBackground(label.getBackground());
+
+                    text.setMargin(new Insets(20, 20, 20, 20));
+
+                    text.addHyperlinkListener(new HyperlinkListener() {
+                        @Override
+                        public void hyperlinkUpdate(HyperlinkEvent e) {
+                            if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED))
+                                try {
+                                    Desktop.getDesktop().browse(e.getURL().toURI());
+                                } catch (Exception ignored) {
+                                }
                         }
+                    });
+
+                    final JFrame frame = new JFrame("Java 8 required");
+
+                    JButton button = new JButton("Exit");
+                    button.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            frame.dispose();
+                        }
+                    });
+
+                    JPanel panel = new JPanel();
+                    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+                    panel.add(text);
+                    panel.add(button);
+                    panel.add(Box.createVerticalStrut(20));
+
+                    frame.setContentPane(panel);
+                    frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+                    frame.setResizable(false);
+                    frame.addWindowListener(new WindowAdapter() {
+                        @Override
+                        public void windowClosed(WindowEvent e) {
+                            synchronized (mutex) {
+                                mutex.notify();
+                            }
+                        }
+                    });
+
+                    frame.pack();
+                    frame.setLocationRelativeTo(null);
+                    frame.setVisible(true);
+                    frame.setAlwaysOnTop(true);
                 }
             });
-            ep.setEditable(false);
-            ep.setBackground(label.getBackground());
 
-            JOptionPane.showMessageDialog(null, ep, "Java 8 required", JOptionPane.ERROR_MESSAGE);
+            //noinspection SynchronizationOnLocalVariableOrMethodParameter
+            synchronized (mutex) {
+                try {
+                    mutex.wait();
+                } catch (InterruptedException e) {
+                    //ignore
+                }
+            }
         }
+
         if (exit) {
             try {
                 Class<?> clazz = Class.forName("java.lang.Shutdown");
