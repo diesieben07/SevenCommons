ab4a42d51f9543329d95c7e890edf0917f42dd67
diesieben07 <diesieben07@gmail.com>
2015-09-24 16:39:25 +0200

    Read GradleStart csvdir property instead of finding it ourselves

--- src/main/java/de/take_weiland/mods/commons/asm/MCPNames.java
+++ src/main/java/de/take_weiland/mods/commons/asm/MCPNames.java
@@ -1,17 +1,14 @@
 package de.take_weiland.mods.commons.asm;
 
-import com.google.common.base.CharMatcher;
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import com.google.common.io.Files;
 import com.google.common.io.LineProcessor;
-import cpw.mods.fml.common.launcher.FMLTweaker;
 import de.take_weiland.mods.commons.internal.SevenCommons;
 import net.minecraft.launchwrapper.Launch;
 
 import javax.annotation.Nullable;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -31,7 +28,8 @@ public final class MCPNames {
     private static final Map<String, String> fields;
     private static final Map<String, String> methods;
 
-    private static final String SYS_PROP = "sevencommons.mappingsFile";
+    private static final String MY_SYS_PROP = "sevencommons.mappingsFile";
+    private static final String FG_SYS_PROP = "net.minecraftforge.gradle.GradleStart.csvDir";
 
     private static final boolean DEV_ENV;
 
@@ -39,48 +37,13 @@ public final class MCPNames {
         DEV_ENV = (Boolean) Launch.blackboard.get("fml.deobfuscatedEnvironment");
         if (use()) {
             String mappingsDir;
-            String prop = System.getProperty(SYS_PROP);
+            String prop = System.getProperty(MY_SYS_PROP);
             if (prop == null) {
-                // find the build.gradle by searching upwards from the CWD
-                File cwd = new File("").getAbsoluteFile();
-                File buildGradle = null;
-                while (cwd != null && cwd.isDirectory()) {
-                    File bg = new File(cwd, "build.gradle");
-                    if (bg.isFile()) {
-                        buildGradle = bg;
-                        break;
-                    }
-                    cwd = cwd.getParentFile();
-                }
-                if (buildGradle == null) {
-                    throw failFindMappings("Could not find build.gradle");
-                }
-
-                // find any mappings= setting in the build.gradle
-                String mappings = tryFindMappings(buildGradle);
-                if (mappings == null) {
-                    // no mappings= setting, mappings are where the forge jar is
-                    File forgeJar = new File(FMLTweaker.getJarLocation());
-                    mappingsDir = forgeJar.getParent() + "/unpacked/conf/";
+                String fgProp = System.getProperty(FG_SYS_PROP);
+                if (fgProp == null) {
+                    throw failFindMappings("Please use GradleStart as your main class.");
                 } else {
-                    // get the gradle cache
-                    File gradleCache;
-                    String overriddenGradleDir = System.getenv("GRADLE_USER_HOME");
-                    if (overriddenGradleDir == null) {
-                        gradleCache = new File(System.getProperty("user.home") + "/.gradle/");
-                    } else {
-                        gradleCache = new File(overriddenGradleDir);
-                    }
-                    if (!gradleCache.isDirectory()) {
-                        throw failFindMappings(String.format("Failed to find gradle cache (tried %s)", gradleCache.getAbsolutePath()));
-                    }
-
-                    // determine the path inside the cache
-                    String[] split = mappings.split("_");
-                    if (split.length != 2) {
-                        throw failFindMappings("Invalid mappings setting in build.gradle: " + mappings);
-                    }
-                    mappingsDir = gradleCache.getAbsolutePath() + "/caches/minecraft/de/oceanlabs/mcp/mcp_" + split[0] + "/" + split[1] + "/";
+                    mappingsDir = fgProp;
                 }
             } else {
                 // have the system property defined
@@ -91,24 +54,6 @@ public final class MCPNames {
             methods = readMappings(new File(mappingsDir + "/methods.csv"));
         } else {
             methods = fields = null;
-        }
-    }
-
-    private static String tryFindMappings(File buildGradle) {
-        try (BufferedReader reader = Files.newReader(buildGradle, StandardCharsets.UTF_8)) {
-            String line;
-            String mappings = null;
-            CharMatcher matcher = CharMatcher.WHITESPACE.or(CharMatcher.is('"'));
-            while ((line = reader.readLine()) != null) {
-                line = matcher.removeFrom(line);
-                if (line.startsWith("mappings=")) {
-                    mappings = line.substring(9, line.length() - (line.endsWith(";") ? 1 : 0));
-                    break;
-                }
-            }
-            return mappings;
-        } catch (IOException e) {
-            throw failFindMappings("IOException reading build.gradle", e);
         }
     }
 
